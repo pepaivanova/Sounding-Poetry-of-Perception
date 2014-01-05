91edf6f5e49d344dadc83f7b6914a0bf99e5fae8
Radoslav Borisov <radoslav.borisov@gmail.com>
2014-01-05 16:35:06 +0200

    Remove useless app, replace with meaningful code

--- web/app.py
+++ web/app.py
@@ -1,58 +1,34 @@
-import os.path as p
-
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
-from sqlalchemy.orm import scoped_session, sessionmaker, relationship
-from sqlalchemy.ext.declarative import declarative_base
-
-from flask import Flask, render_template, request, redirect, url_for
-
+from flask import Flask
+from flask import render_template
+from flask import request
+from send2Pd import send2Pd 
+from send2Pd import audioOn
 app = Flask(__name__)
-db_file = p.abspath(p.join(p.dirname(__file__), 'sounds.db'))
-
-engine = create_engine('sqlite:////%s' % (db_file, ), convert_unicode=True)
-db_session = scoped_session(sessionmaker(autocommit=False,
-                                         autoflush=False,
-                                         bind=engine))
-
-Base = declarative_base()
-
-def create_db():
-    Base.metadata.create_all(bind=engine)
 
 
-class Sound(Base):
-    __tablename__ = 'sounds'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50), unique=True)
-    filepath = Column(String(50), )
+@app.route('/', methods=['POST', 'GET'])
+def poetry(name=None):
+    error = None
+    if request.method == 'POST':
+	# get text from the form 
+        text=request.form['poetry']
+        # turn on audio
+        audioOn()
+        # send text to pure data
+        send2Pd(text)
+        # show text on the bottom of page
+        return render_template('index.html', result=text)
+    else:
+        error = 'Error !'
+    #
+    return render_template('index.html', name=name)
 
-
-class Word(Base):
-    __tablename__ = 'words'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50), unique=True)
-    sound_id = Column(Integer, ForeignKey('sounds.id'))
-
-
-
-@app.teardown_appcontext
-def shutdown_session(exception=None):
-    db_session.remove()
-
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/', methods=['POST'])
-def play_poetry():
-    # TODO: process poetry here, send to PD
-
-    redirect('/')
-
-
-
+@app.route('/about')
+def about():
+    # open About page, where should be described the project
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.debug = True
+    app.run(host='0.0.0.0')
