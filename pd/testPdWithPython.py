93f02a5284adeb0f0d4c52e266c8fc574a6c298b
Radoslav Borisov <radoslav.borisov@gmail.com>
2014-01-16 15:10:11 +0200

    updated to work with the latest patch

--- pd/testPdWithPython.py
+++ pd/testPdWithPython.py
@@ -2,17 +2,21 @@ import os
 
 # (Your own Python script that does whatever you need)
 
+
 def send2Pd(message=''):
     # Send a message to Pd
-    os.system("echo '" + message + "' | pdsend 3000")
+    os.system("echo '" + message + ";" + "' | pdsend 3000")
+
 
 def dspOn():
-    message = 'dspOn;'    # DSP turns On
+    message = 'dspOn'    # DSP turns On
     send2Pd(message)
 
+
 def dspOff():
-    message = 'dspOff;'   # DSP turns Off
+    message = 'dspOff'   # DSP turns Off
     send2Pd(message)
+
 
 def setVolume():
     vol = 80    # Set volume value (0-100)
@@ -21,5 +25,6 @@ def setVolume():
 
 if __name__ == "__main__":
     dspOn()
-    send2Pd("play;")
+    send2Pd("read wav/motorr.wav array1")
+    send2Pd("0, 260000 5892")
     # dspOff()
