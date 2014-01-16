844f52165a0a5399054e04dceab2257171424396
Radoslav Borisov <radoslav.borisov@gmail.com>
2014-01-16 14:11:19 +0200

    sound off button is working correctly now

--- web/app.py
+++ web/app.py
@@ -27,32 +27,47 @@ else:
 def poetry(name=None):
     error = None
     if request.method == 'POST':
-	# get text from the form
-        text=request.form['poetry']
-        database.store_poetry(text)
+    # get text from the form
+        text = request.form['poetry']
+        if text != "Please enter text.":
+            database.store_poetry(text)
         # turn on audio
-        dspOn()
+        try:
+            request.form['sound_off'] == "Sound OFF"
+            dspOff()
+        except:
+            dspOn()
         # send text to pure data
         send2Pd(text)
-        # if Stop sound pressed send dspOff() message
-        # if request.form['sound_off'] == 'Sound OFF':
-        #    dspOff()
-        # show text on the bottom of page
         return render_template('index.html', result=text)
+        #
     else:
         error = 'Error !'
     #
     return render_template('index.html', name=name)
+
+
+@app.route('/config', methods=['POST', 'GET'])
+def config():
+    if request.method == 'POST':
+        if request.form['sound_off'] == "Sound OFF":
+            dspOff()
+            print("Sound OFF")
+    # config page, where sounds could be started and stoped
+    return render_template('config.html')
+
 
 @app.route('/about')
 def about():
     # open About page, where should be described the project
     return render_template('about.html')
 
+
 @app.route('/history')
 def history():
     all_ = database.db_session.query(database.Poetry).all()
     return '<br>'.join(p.poetry for p in all_)
+
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
