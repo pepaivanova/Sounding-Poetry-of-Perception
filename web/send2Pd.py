5fe06c1be6268714511e6272257fb3a2bdb675aa
Radoslav Borisov <radoslav.borisov@gmail.com>
2014-01-17 14:06:22 +0200

    processText() function added to play sound from a list

--- web/send2Pd.py
+++ web/send2Pd.py
@@ -18,6 +18,15 @@ def send2Pd(message=''):
     # Send a message to Pd
     os.system("echo '" + message + ";" + "' | pdsend 3000")
 
+def processText(text):
+    # Get text from flask and send it to pd
+    words = text.split()
+    for i in range(len(words)):
+        send2Pd("read wav/" + words[i] + ".wav array1")
+        send2Pd("0, 260000 5892")
+    # print(words)
+    print("Done")
+
 def dspOn():
     message = 'dspOn'    # DSP turns On
     send2Pd(message)
@@ -30,4 +39,6 @@ if __name__ == "__main__":
     dspOn()
     send2Pd("play;")
     # dspOff()
+    text = "I hear motorr while we are walking. "
+    processText(text)
     print(checkPdStarted('pdextended'))
