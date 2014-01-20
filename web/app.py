a946377d589c034f9bd25be0db072bf5276ead3a
Radoslav Borisov <radoslav.borisov@gmail.com>
2014-01-20 17:37:06 +0200

    added basic functionality to send and receive messages to/from PD for given time

--- web/app.py
+++ web/app.py
@@ -1,5 +1,7 @@
 import os
 import os.path as p
+from time import time, sleep
+from os import path, getcwd
 
 from flask import Flask
 from flask import render_template
@@ -26,7 +28,27 @@ def startPd():
     print("PD started")
     pd.Send(["Hello", "World"])
     '''
-    pass
+    print("Starting PD")
+    start = time()
+    # launching pd
+    pd = Pd(nogui=False)
+    pd.Send(["test message", 1, 2, 3])
+
+    def Pd_hello(self, message):
+        print "Pd called Pd_hello(%s)" % message
+
+    pd.Pd_hello = Pd_hello
+
+    sentexit = False
+    # running a bunch of stuff for up to 20 seconds
+    while time() - start < 60 and pd.Alive():
+        if time() - start > 20 and not sentexit:
+            pd.Send(["exit"])
+            sentexit = True
+        pd.Update()
+
+    if pd.Alive():
+        pd.Exit()
 
 
 def getCurrentDateTime():
