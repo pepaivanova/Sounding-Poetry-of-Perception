d3f6d873634437fea7c7132c9bff9818922cc92c
Radoslav Borisov <radoslav.borisov@gmail.com>
2014-01-21 16:31:33 +0200

    working version with two files

--- web/test.py
+++ web/test.py
@@ -3,13 +3,13 @@ from pd import Pd
 
 start = time()
 # start pd
-pd = Pd(port=3010, nogui=False, open="sounding.pd", cmd=None,
+pd = Pd(port=3010, nogui=True, open="sounding.pd", cmd=None,
         path=["patches"], extra=None, stderr=False)
 pd.Send(["Hello world"])
 pd.Send(["Hello Rado"])
 pd.Send(["dspOn"])
-pd.Send(["dspOff"])
-pd.Send(["dspOn"])
+#pd.Send(["dspOff"])
+#pd.Send(["dspOn"])
 pd.Send(["load read sounds/moga.wav array1"])
 pd.Send(["play1"])
 pd.Send(["load read sounds/motorr.wav array2"])
@@ -17,8 +17,10 @@ pd.Send(["play2"])
 
 
 while pd.Alive():
-    pd.Update()
+    data = pd.Update()
 
+if pd.Alive():
+    pd.Exit()
 '''
 sentexit = False
 while time() - start < 20 and pd.Alive():
