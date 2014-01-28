3bdc4203a91d3ab61e781520875f8b1446086272
Radoslav Borisov <radoslav.borisov@gmail.com>
2014-01-28 18:09:45 +0200

    play sounds and exit gracefully

--- web/processData.py
+++ web/processData.py
@@ -1,11 +1,13 @@
 # process messages from PD
 
+
 def processData(data):
     #
     if data is not None:
         print(data)
-        if 'play' in data:
+        if 'play-1' in data:
             return "load2 read sounds/motorr.wav array2"
+        elif 'play-2' in data:
+            return "load1 read sounds/moga.wav array1"
         else:
-            print('error')
-
+            return "exit"
