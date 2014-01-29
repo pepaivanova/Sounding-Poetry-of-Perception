1e8955e9d9f7e94c12d0f507f0482722a20b4880
Radoslav Borisov <radoslav.borisov@gmail.com>
2014-01-29 14:59:56 +0200

    small changes for better understanding

--- web/sounding.py
+++ web/sounding.py
@@ -1,23 +1,8 @@
-from time import time
+#from time import time
 from pd import Pd
 
+
 def startPd():
-    #start = time()
-    # start pd
-    pd = Pd(port=3010, nogui=True, open="sounding.pd", cmd=None,
-            path=["patches"], extra=None, stderr=False)
-    pd.Send(["Hello world"])
-    pd.Send(["Hello Rado"])
-    pd.Send(["dspOn"])
-    #pd.Send(["dspOff"])
-    #pd.Send(["dspOn"])
-    pd.Send(["load1 read sounds/moga.wav array1"])
-
-    while pd.Alive():
-        pd.Update()
-
-    if pd.Alive():
-        pd.Exit()
     '''
     sentexit = False
     while time() - start < 20 and pd.Alive():
@@ -31,6 +16,20 @@ def startPd():
     if pd.Alive():
         pd.Exit()
     '''
+    #start = time()
+    # start pd
+    pd = Pd(port=3010, nogui=True, open="sounding.pd", cmd=None,
+            path=["patches"], extra=None, stderr=False)
+    pd.Send(["dspOn"])
+    pd.Send(["dspOff"])
+    pd.Send(["dspOn"])
+    pd.Send(["load1 read sounds/moga.wav array1"])
+
+    while pd.Alive():
+        pd.Update()
+
+    if pd.Alive():
+        pd.Exit()
 
 
 if __name__ == "__main__":
