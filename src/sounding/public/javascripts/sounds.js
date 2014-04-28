c9e33c5d6a9b1508faa607aae98c87577db93a8f
Radoslav Borisov <radoslav.borisov@gmail.com>
2014-04-28 22:44:38 +0300

    play sound from home server

--- src/sounding/public/javascripts/sounds.js
+++ src/sounding/public/javascripts/sounds.js
@@ -1,13 +1,16 @@
-var mySound = new buzz.sound( "/sounds/rain-01", {
-    formats: [ "ogg", "mp3", "aac" ],
-    preload: false,
+var mySound = new buzz.sound( "http://192.168.1.1:85/other/freeSounds/01/101389.wav", {
+    // formats: [ "mp3", "ogg", "aac" ],
+    preload: true,
     autoplay: true,
     loop: false
 });
 
 mySound.play()
+    .fadeIn();
+/*
     .fadeIn()
     .bind( "timeupdate", function() {
        var timer = buzz.toTimer( this.getTime() );
        document.getElementById( "timer" ).innerHTML = timer;
     });
+*/
