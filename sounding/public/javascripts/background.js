872e13753d425024801886288465f08abc37d8bf
Radoslav Borisov <radoslav.borisov@gmail.com>
2014-05-07 18:59:56 +0300

    Working application using express. Simple as it should be.

---
+++ sounding/public/javascripts/background.js
@@ -0,0 +1,25 @@
+// load background images as backgrounds or slideshow
+$(function() {
+        $.vegas({
+            preload: true,
+            delay: 8000,
+            fade: 4000,
+            src:'images/background_Ljudmila.jpg'
+        });
+        $.vegas('overlay', {
+            src:'javascripts/vegas/overlays/13.png'
+        });
+});
+/*
+$( function() {
+    $.vegas( 'slideshow', {
+        preload: true,
+        delay: 8000,
+        backgrounds: [
+            { src: 'images/background1.jpg', fade: 4000 },
+            { src: 'images/background2.jpg', fade: 4000 },
+        ]
+    } )('overlay');
+});
+*/
+
