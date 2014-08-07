393dc73dbf2a6b857451a4f15d8ccbe10e418bae
Radoslav Borisov <radoslav@rdrlab.(none)>
2014-08-07 15:51:34 +0300

    all sound folders added to path

--- sounding/couchdb/config.js
+++ sounding/couchdb/config.js
@@ -2,3 +2,17 @@
 // define database name in couchdb
 var dbName = 'sounding';
 
+// paths to json files
+var config = {
+        path1 : 'http://home.rdrlab.com:85/other/freeSounds/01/',
+	path2 : 'http://home.rdrlab.com:85/other/freeSounds/02/',
+	path3 : 'http://home.rdrlab.com:85/other/freeSounds/03/',
+	path4 : 'http://home.rdrlab.com:85/other/freeSounds/04/',
+	path5 : 'http://home.rdrlab.com:85/other/freeSounds/05/',
+	path6 : 'http://home.rdrlab.com:85/other/freeSounds/06/',
+	path7 : 'http://home.rdrlab.com:85/other/freeSounds/07/',
+	path8 : 'http://home.rdrlab.com:85/other/freeSounds/08/',
+	path9 : 'http://home.rdrlab.com:85/other/freeSounds/09/',
+	path10 : 'http://home.rdrlab.com:85/other/freeSounds/10/'
+};
+
