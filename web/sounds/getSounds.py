f111555fabb06b4c876ce205dcf5144a4b33495f
Radoslav Borisov <radoslav.borisov@gmail.com>
2014-09-08 15:59:51 +0300

    Automatic download and rename sounds from server

---
+++ web/sounds/getSounds.py
@@ -0,0 +1,26 @@
+import json
+from subprocess import call
+
+def getAllSounds(fileName):
+    #
+    # get keyTags
+    call(["wget http://home.rdrlab.com:5984/sounding/_design/sounding/_view/keyTags -O " + fileName], shell=True )
+    processKeyTags(fileName)
+
+def processKeyTags(fileName):
+    #
+    jj = json.load(open(fileName))
+    print(jj.keys())
+    # print(jj["rows"])
+    # for i in range(len(jj["rows"])):
+    for i in range(3):
+        #print(jj["rows"][i]["key"] + " - " + jj["rows"][i]["value"])
+        downloadWav(jj["rows"][i]["value"], jj["rows"][i]["key"] + ".wav")
+
+def downloadWav(url, name):
+    #
+    call(["wget " + url + " -O " + name], shell=True)
+
+if __name__ == "__main__":
+    sounds = getAllSounds("keyTags.json")
+    # print(sounds)
